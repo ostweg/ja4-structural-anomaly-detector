8e5801fa87879591f634f5f2f0b3e41df52468f3
sgapriv <sgapriv@Mac.localdomain>
2026-05-27 10:52:44 +0200

    Updated data labeling

--- data.py
+++ data.py
@@ -22,8 +22,8 @@ def load_bad_bot_list():
     bad_bots = response.text.splitlines()
 
     exclude_list = {
-        'hatGPT-User',
-        'openai',
+        'ChatGPT-User',
+        'openai.com',
         'Netcraft',
         'MicroMessenger',
         'Bytespider',
@@ -35,14 +35,20 @@ def load_bad_bot_list():
      
     return bad_bots
 
-def is_known_crawler(df):
+def is_known_crawler(df,row_index):
+    df = df.copy()
 
     df['is_known_crawler'] = False
     df['crawler_match'] = None
-    df['bad_origin'] = None
 
-    df = df.copy()
     crawlers = load_crawler_list()
+
+    if 'is_known_bad_bot' in df.columns and 'is_known_browser' in df.columns:
+        mask = (df['is_known_bad_bot'] == False) & (df['is_known_browser'] == False)
+    
+    if mask.any():
+        first_indices = df[mask].index[:row_index]
+        mask = df.index.isin(first_indices)
 
     def _check_row(ua_string):
         ua_lower = ua_string.lower()
@@ -65,21 +71,28 @@ def is_known_crawler(df):
                         return True, pat
         return False, None
     
-    results = df['user_agent_string'].progress_apply(_check_row)
-    df['is_known_crawler'] = [r[0] for r in results]
-    df['crawler_match'] = [r[1] for r in results]
-    df.loc[df['is_known_crawler'] == True, 'bad_origin'] = 0
+    if mask.any():
+        print('Processing known crawler...')
+        results = df.loc[mask, 'user_agent_string'].progress_apply(_check_row)
+        df.loc[mask, 'is_known_crawler'] = [r[0] for r in results]
+        df.loc[mask, 'crawler_match'] = [r[1] for r in results]
+        df.loc[df['is_known_crawler'] == True, 'bad_origin'] = 0
 
     return df
 
-def is_known_browser(df):
+def is_known_browser(df,row_index):
    df = df.copy()
 
    df['is_known_browser'] = False
    df['browser_match'] = None
    
-   if 'is_known_crawler' in df.columns:
-       mask = df['is_known_crawler'] == False
+   if 'is_known_bad_bot' in df.columns:
+       mask = df['is_known_bad_bot'] == False
+       
+
+   if mask.any():
+       first_indices = df[mask].index[:row_index]
+       mask = df.index.isin(first_indices)
 
    def _check_row(ua_string):
     parsed = parse_ua(ua_string)
@@ -89,6 +102,7 @@ def is_known_browser(df):
     return False, None
    
    if mask.any():
+    print('Processing known browsers...')
     results = df.loc[mask, 'user_agent_string'].progress_apply(_check_row)
     df.loc[mask, 'is_known_browser'] = [r[0] for r in results]
     df.loc[mask, 'browser_match'] = [r[1] for r in results]
@@ -100,6 +114,7 @@ def is_known_bad_bot(df):
 
     df['is_known_bad_bot'] = False
     df['bad_bot_match'] = None
+    df['bad_origin'] = None
 
     bad_bots = load_bad_bot_list()
     bad_bots_paired = [(bot.lower(), bot) for bot in bad_bots]
@@ -114,20 +129,17 @@ def is_known_bad_bot(df):
                 return True, bot_orig
         return False, None   
     
-    # if mask.any():  
-    #    results = df.loc[mask, 'user_agent_string'].progress_apply(check_ua)
-    #    df.loc[mask, 'is_known_bad_bot'] = [r[0] for r in results]
-    #    df.loc[mask, 'bad_bot_match'] = [r[1] for r in results]
-    #    df.loc[df['is_known_bad_bot'] == True, 'bad_origin'] = 1
-
+    print('Processing known bad bots...')
     results = df['user_agent_string'].progress_apply(check_ua)
     df['is_known_bad_bot'] = [r[0] for r in results]
     df['bad_bot_match'] = [r[1] for r in results]
+    df.loc[df['is_known_bad_bot'] == True, 'bad_origin'] = 1
+
     return df
 
 def main():
     INPUT_PATH = Path('data/test/ja4+_db.json')
-    OUTPUT_PATH = Path('data/test/ja4_labeled_bad_bots.csv')
+    OUTPUT_PATH = Path('data/test/ja4+_labeled.csv')
 
     df = load_ja4plus_json(INPUT_PATH)
     pd.options.display.max_colwidth = 300
@@ -136,14 +148,16 @@ def main():
     df = df.dropna(subset=['user_agent_string'])
     
     df = is_known_bad_bot(df)
-    # df = is_known_crawler(df)
-    # df= is_known_browser(df)
+    df= is_known_browser(df,250)
+    df = is_known_crawler(df,500) 
 
     print(df['bad_bot_match'].value_counts())
 
     print('Total bad bots:',len(df[df['is_known_bad_bot'] == True]))
-    # print('Unkown:',len(df[(df['is_known_bad_bot'] == False)&(df['is_known_crawler'] == False) & (df['is_known_browser'] == False)]))
+    print('Unknown:',len(df[(df['is_known_bad_bot'] == False)&(df['is_known_crawler'] == False) & (df['is_known_browser'] == False)]))
 
+    df.dropna(subset=['bad_origin'], inplace=True)
+    df = df[['ja4_fingerprint','ja4h_fingerprint','bad_origin']]
     df.to_csv(OUTPUT_PATH, index=False)
 
 if __name__ == '__main__':
